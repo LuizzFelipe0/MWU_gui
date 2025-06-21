18e1c57b7e077e9300134d9dbb9ac166e11d613a
Luiz Felipe <lfcosta@faeterj-petropolis.edu.br>
2025-06-21 00:08:18 -0300

    Add user_accounts endpoints

--- core/user_accounts_endpoints.py
+++ core/user_accounts_endpoints.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from ..core.api_client import APIClient
+from core.api_client import APIClient
 
 
 class UserAccountsAPIClient(APIClient):
@@ -11,20 +11,11 @@ class UserAccountsAPIClient(APIClient):
     def get_users_accounts_by_id(self, users_accounts_id: UUID):
         return self._make_request("GET", f"/users_accounts/{users_accounts_id}")
 
-    def create_users_accounts(self, users_accounts_data: dict):
-        return self._make_request("POST", "/users_accounts/create", json=users_accounts_data)
+    def create_user_accounts(self, user_id: UUID, account_id: UUID):
+        return self._make_request("POST", f"/users_accounts/users/{user_id}/accounts/{account_id}")
 
-    def update_users_accounts(self, users_accounts_id: UUID, users_accounts_data: dict):
-        return self._make_request("PATCH", f"/users_accounts/{users_accounts_id}/update", json=users_accounts_data)
-
-    def delete_users_accounts(self, users_accounts_id: UUID):
-        return self._make_request("DELETE", f"/users_accounts/{users_accounts_id}/delete")
-
-    def restore_users_accounts(self, users_accounts_id: UUID):
-        return self._make_request("POST", f"/users_accounts/{users_accounts_id}/restore")
-
-    def force_delete_users_accounts(self, users_accounts_id: UUID):
-        return self._make_request("DELETE", f"/users_accounts/{users_accounts_id}/force-delete")
+    def delete_users_accounts(self, user_id: UUID, account_id: UUID):
+        return self._make_request("DELETE", f"/users_accounts/users/{user_id}/accounts/{account_id}")
 
 
 users_accounts_api_client = UserAccountsAPIClient()
